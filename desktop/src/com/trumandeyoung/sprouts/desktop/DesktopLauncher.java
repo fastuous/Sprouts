575d722297d7b99378b2e76115737ad181a631c5
Truman DeYoung <tdeyoung@unm.edu>
2015-02-18 12:04:55 -0700

    16:9 aspect ration on desktop. Test commit.

--- desktop/src/com/trumandeyoung/sprouts/desktop/DesktopLauncher.java
+++ desktop/src/com/trumandeyoung/sprouts/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.trumandeyoung.sprouts.Sprouts;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 900;
+		config.width = 506;
 		new LwjglApplication(new Sprouts(), config);
 	}
 }
