23c4988c954564dbf3f6a6f6c3b784851aa441c5
Truman DeYoung <tdeyoung@unm.edu>
2015-02-18 00:23:08 -0700

    Initial commit

---
+++ android/src/com/trumandeyoung/sprouts/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.trumandeyoung.sprouts.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.trumandeyoung.sprouts.Sprouts;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Sprouts(), config);
+	}
+}
