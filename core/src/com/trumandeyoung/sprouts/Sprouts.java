4ddc2c5eb42e2109251b44a676458c5144124d26
Truman DeYoung <tdeyoung@unm.edu>
2015-02-22 14:17:06 -0700

    Starting path experimentation.

--- core/src/com/trumandeyoung/sprouts/Sprouts.java
+++ core/src/com/trumandeyoung/sprouts/Sprouts.java
@@ -7,12 +7,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Bezier;
+import com.badlogic.gdx.math.Vector2;
 
 public class Sprouts extends ApplicationAdapter {
 	SpriteBatch batch;
+	ShapeRenderer renderer;
 	BitmapFont font;
 	CharSequence str;
 	Texture img;
+	boolean touched;
 	
 	@Override
 	public void create () {
@@ -26,17 +31,36 @@ public class Sprouts extends ApplicationAdapter {
 		res += " Height: " + Gdx.graphics.getHeight();
 		res += " Density: " + Gdx.graphics.getDensity();
 		str = res;
+		
+		touched = false;
 	}
 
 	@Override
 	public void render () {
+		Vector2 p1;
+		Vector2 p2;
+		boolean path = false;
+		
+		if (!touched && Gdx.input.justTouched()) {
+			p1 = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+			touched = true;
+		} else if (Gdx.input.justTouched())
+		{
+			p2 = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+			path = true;
+		}
+		Bezier<Vector2> p = new Bezier<Vector2>(p1, p2);
+		if (path) {
+			
+		}
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		int x = Gdx.input.getX();
 		int y = Gdx.input.getY();
 		if(Gdx.input.justTouched()) Gdx.input.vibrate(100);
-		batch.begin();
-		font.draw(batch, str, x, Gdx.graphics.getHeight() - y);
-		batch.end();
+		
+		renderer.begin();
+		
 	}
 }
